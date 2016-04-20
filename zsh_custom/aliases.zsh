509d6b82de57a5b976fff329ec7501b248dfed0a
Nico Nistal <nicolas.nistal@thegoneapp.com>
2016-04-20 14:23:41 -0300

    Set vim/macvim as the default editor and add custom alias

---
+++ zsh_custom/aliases.zsh
@@ -0,0 +1,23 @@
+#!zsh
+#Aliases file
+#
+
+alias composer='php /usr/local/bin/composer.phar'
+
+# ----------------------------------------- #
+# Git aliases                               #
+# ----------------------------------------- #
+
+alias gs='git status'
+
+# ----------------------------------------- #
+# Folder Shortcuts                          #
+# ----------------------------------------- #
+
+alias gone='cd ~/Developer/Gone'
+
+alias vim='/usr/local/Cellar/vim/7.4.898/bin/vim'
+
+alias tandil='curl wttr.in/Tandil'
+
+alias mardel='curl wttr.in/mdq'
